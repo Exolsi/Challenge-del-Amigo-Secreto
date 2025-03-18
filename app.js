be00ff634a8f461e6f645f178b3666c5a180bb87
Exolsi <gabrielslucian46@gmail.com>
2025-03-17 20:12:50 -0400

    Actualiza app.js

--- app.js
+++ app.js
@@ -2,4 +2,14 @@ let nombres = [];
 
 function agregarNombre() {
     let nombre = prompt("Ingresa el nombre de un amigo:").trim();
+    if (nombre === "") {
+        alert("Por favor, ingresa un nombre válido.");
+        return;
+    }
+    if (!nombres.includes(nombre)) {
+        nombres.push(nombre);
+        actualizarLista();
+    } else {
+        alert("Este nombre ya ha sido agregado.");
+    }
 }
